e3e71449bfaac6012bd9283e6c500322df7b5640
Criis21 <cristina_kirika@hotmail.com>
2022-01-19 17:49:35 +0000

    Final exercise!

--- src/app.py
+++ src/app.py
@@ -1,24 +1,24 @@
-from flask import Flask
+from flask import Flask, jsonify, request, json
 app = Flask(__name__)
+
+todos = [{"label": "My first task", "done": False}]
 
 @app.route('/todos', methods=['GET'])
 def hello_world():
-    # supongamos que tienes some_data (cierta información) en una variable json
-    some_data = { "name": "Bobby", "lastname": "Rixer" }
+    return jsonify(todos)
 
-    # puedes convertir esa variable en un string json así
-    json_text = flask.jsonify(some_data)
+@app.route('/todos', methods=['POST'])
+def add_new_todo():
+    decoded_object = json.loads(request.data)
+    todos.append(decoded_object)
+    print("Incoming request with the following body", request.data)
+    return jsonify(todos)
 
-    # y luego puedes retornarla (return) en el response body así:
-    return flask.jsonify
-
-
-todos = [
-    { "label": "My first task", "done": False },
-    { "label": "My second task", "done": False }
-]
-
-# Estas dos líneas siempre seben estar al final de tu archivo app.py.
+@app.route('/todos/<int:position>', methods=['DELETE'])
+def delete_todo(position):
+    todos.pop(position)
+    print("This is the position to delete: ",position)
+    return jsonify(todos)
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=3245, debug=True)
+    app.run(host='0.0.0.0', port=3245, debug=True)
